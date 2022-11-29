c96a8f2c5e879db0da9cb183dbfa0f4997da1f08
Michael Aloh <mykelaloh@gmail.com>
2022-11-28 21:35:43 -0800

    updated 5

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,8 @@
+/**
+ *check_cycle - checks foe a cycle in a linked list
+ *@list:pointer to the head of tge list
+ *Return: 1 if cycle is present and 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(3 * {}.format{str})
-print(str[:9])
+print(f"{3 * str}\n{str[:9]}")
