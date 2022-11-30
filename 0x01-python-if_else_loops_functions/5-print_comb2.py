53a92b681231e91cbf56c58b8ab25c16ffe75194
Michael Aloh <mykelaloh@gmail.com>
2022-11-30 14:16:54 -0800

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        print("{i}{j}".format(i = i, j = j), end="")
+        print("{i}{j}".format(i=i, j=j), end="")
         if (i != 9 or j != 9):
-            print(", ", end="")
+            print(", ".format(), end="")
         else:
-            print("\n",end="")
+            print("\n".format(), end="")
