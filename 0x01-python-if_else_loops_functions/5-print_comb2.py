fd113467a53a2deb98f09277348fc60adcc7091e
Michael Aloh <mykelaloh@gmail.com>
2022-11-30 13:41:29 -0800

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        print(f"{i}{j}", end="")
+        print("{i}{j}".format(i = i, j = j), end="")
         if (i != 9 or j != 9):
             print(", ", end="")
+        else:
+            print("\n",end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if (j > i):
-            print(f"{i}{j}", end="")
+            print("{i}{j}".format(i = i, j = j), end="")
             if (i != 8 or j != 9):
                 print(", ", end="")
