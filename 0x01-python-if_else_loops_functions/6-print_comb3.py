93cdc0b7f7faf3d457588d300223b0e60f0a9c50
Michael Aloh <mykelaloh@gmail.com>
2022-11-30 19:13:05 -0800

    done wuth task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if (j > i):
-            print("{i}{j}".format(i = i, j = j), end="")
-            if (i != 8 or j != 9):
-                print(", ", end="")
+        if (j > i and (i != 8 or j != 9)):
+            print("{}{}".format(i, j), end=", ")
+        elif (i == 8 and j == 9):
+            print("{}{}\n".format(i, j))
