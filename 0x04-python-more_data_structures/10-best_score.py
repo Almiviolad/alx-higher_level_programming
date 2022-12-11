0fc948a7dcfd78d1f3b2737a7afc361058300cef
Michael Aloh <mykelaloh@gmail.com>
2022-12-11 10:09:38 -0800

    done wirh 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max_name = None
+    if (a_dictionary == {}):
+        return max_name
     for i in a_dictionary:
         max = a_dictionary[i]
         break
     for i in a_dictionary:
         if (a_dictionary[i] > max):
             max = a_dictionary[i]
-            res = i
-    return res
+            max_name = i
+    return max_name
