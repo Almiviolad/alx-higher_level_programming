826add54741f3d1b6615ea004ce011ac1d4e8fb9
Michael Aloh <mykelaloh@gmail.com>
2023-01-13 08:18:23 -0800

    done with task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Functoon to openand print the content of a file"""
+
+
+def read_file(filename=""):
+    """Reads and print line content of a file
+    Args:
+        filename: The name of the fike to be read
+        """
+    with open(filename, encoding="UTF-8") as f:
+        f.read
+        for line in f:
+            print(line)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 repo for higher level programming language
 dir for exceptions tasks
+file for python input and output task
