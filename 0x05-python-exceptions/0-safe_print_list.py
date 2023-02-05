bd3dad0d23dbde49b595fc2ac44ddf209159c124
Michael Aloh <mykelaloh@gmail.com>
2023-02-05 11:00:39 -0800

    0-task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    i = 0  # Counter variable
+def safe_print_list(my_list=[],     i = 0  # Counter variable
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+This is tge repo for objects, liwt and dictionarues opertions
