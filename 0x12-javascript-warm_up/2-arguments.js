413b7cd0569d3d9c43384ed701ec77fbd9e956b6
Michael Aloh <mykelaloh@gmail.com>
2023-04-11 15:44:20 -0700

    dont import module

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-if (argv.length <= 2) {
-  console.log('No argument');
-} else if (argv.length === 3) {
-  console.log('Argument found');
-} else { console.log('Arguments found'); }
+const argc = process.argv.length;
+console.log(argc === 2 ? 'No argument' : argc === 3 ? 'Argument found' : 'Arguments found');
