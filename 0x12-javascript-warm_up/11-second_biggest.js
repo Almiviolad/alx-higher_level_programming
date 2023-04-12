a50874de1dfb9c9ff5323adff72538f1c78880c0
Michael Aloh <mykelaloh@gmail.com>
2023-04-12 00:48:12 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,18 +7,18 @@ let secg = first;
 if (length === 2 || length === 3) {
   console.log(0);
 } else {
-    for (let i = 2; i < length; i++) {
-      let current = parseInt(process.argv[i]);
-	if (current > greatest) {
-	    secg = greatest;
-	greatest = current;}
-	else if ((greatest - current) < diff && greatest - current != 0) {
-	  diff = greatest - current;
-	}
+  for (let i = 2; i < length; i++) {
+    const current = parseInt(process.argv[i]);
+    if (current > greatest) {
+      secg = greatest;
+      greatest = current;
+    } else if ((greatest - current) < diff && greatest - current !== 0) {
+      diff = greatest - current;
     }
-    if (greatest -diff === -Infinity) {
-	console.log(secg);
-    }
-    else {
-	console.log(greatest - diff);}
+  }
+  if ((greatest - diff === -Infinity) || (greatest - diff < secg)) {
+    console.log(secg);
+  } else {
+    console.log(greatest - diff);
+  }
 }
