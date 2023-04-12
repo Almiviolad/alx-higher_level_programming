f34c594555d08a6a2007b409432af884acedb246
Michael Aloh <mykelaloh@gmail.com>
2023-04-12 14:44:47 -0700

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+/**
+*Defines a recatngle class
+*@h: height
+*@w: weught
+*/
 class Rectangle {
-    constructor(w, h) {
-	this.width = w;
-	this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
