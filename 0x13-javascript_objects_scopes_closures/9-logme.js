34ba85586a55639efa49e3ac2a21a50deeea69de
Michael Aloh <mykelaloh@gmail.com>
2023-04-13 03:19:43 -0700

    edited 8

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.logMe = function (item) {
-    let count = 0;
-    return function con(item) {
-	console.log(count + ': ' + item);
-	return (count++);
-    }
-} ();
+exports.logMe = (function (item) {
+  let count = 0;
+  return function con (item) {
+    console.log(count + ': ' + item);
+    return (count++);
+  };
+}());
