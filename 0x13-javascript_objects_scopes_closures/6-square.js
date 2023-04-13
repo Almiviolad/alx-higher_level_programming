c156a1ab29108aeeeb707cbe0817484df81feebd
Michael Aloh <mykelaloh@gmail.com>
2023-04-13 02:15:48 -0700

    task7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ const Square1 = require('./5-square.js');
 class Square extends Square1 {
   charPrint (c) {
     if (!c) {
-      c = 'x';
+	c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       let wid = '';
