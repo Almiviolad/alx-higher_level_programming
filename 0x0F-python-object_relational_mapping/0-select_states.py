ae594c3cb29c3d033d665cf9a89db37e20269bc9
Michael Aloh <mykelaloh@gmail.com>
2023-04-15 15:34:08 -0700

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     """
     Cinnect to tgedatabase abd gets states
     """
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
+    db = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+        host="localhost", port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     states_row = cur.fetchall()
