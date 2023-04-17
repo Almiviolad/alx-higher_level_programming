2e0f7f51fb89e952567d09b96e9f9f1d3ba8a1ca
Michael Aloh <mykelaloh@gmail.com>
2023-04-17 04:35:17 -0700

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all states objects from database hbtn_0e_6_usa"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """connect to database and return query"""
+    db_link = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+    eng = create_engine(db_link)
+    Session = sessionmaker(bind=eng)
+
+    session = Session()
+    f_state = session.query(State).order_by(State_id).first()
+    if (state is not None):
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
