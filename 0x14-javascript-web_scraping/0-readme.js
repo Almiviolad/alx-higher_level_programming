c33101a74f444175fbc99893d0ad80c9def9b348
Aloh Michael <mykelaloh@gmail.com>
2023-05-24 05:03:06 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const filePath = process.argv[2];
 const fs = require('fs');
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.err(err);
+    console.error(err);
     return;
   }
   console.log(data);
